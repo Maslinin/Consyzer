d27a9416738eccb9a9621c3456d142ee13b25422
Danil Rudin <ddanilr@yandex.ru>
2025-06-23 01:02:21 +0300

    refactor: split ServiceCollectionExtensions into partials for future extensibility

---
+++ Consyzer/DependencyInjection/ServiceCollectionExtensions.Misc.cs
@@ -0,0 +1,23 @@
+﻿using Consyzer.Options;
+using Consyzer.Reporting;
+using Consyzer.Reporting.Writers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Consyzer.DependencyInjection;
+
+internal static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddReportWriters(this IServiceCollection services, OutputFormats formats)
+    {
+        if (formats.HasFlag(OutputFormats.Console))
+            services.AddSingleton<IReportWriter, ConsoleReportWriter>();
+
+        if (formats.HasFlag(OutputFormats.Json))
+            services.AddSingleton<IReportWriter, JsonReportWriter>();
+
+        if (formats.HasFlag(OutputFormats.Csv))
+            services.AddSingleton<IReportWriter, CsvReportWriter>();
+
+        return services;
+    }
+}
