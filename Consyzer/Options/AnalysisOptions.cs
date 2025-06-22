9e904d35c9950a54ab0c5b8b7848e72d0ee241e2
Danil Rudin <ddanilr@yandex.ru>
2025-06-23 01:05:09 +0300

    refactor: replace init with set in all config-bound properties

--- Consyzer/Options/AnalysisOptions.cs
+++ Consyzer/Options/AnalysisOptions.cs
@@ -2,8 +2,8 @@
 
 internal sealed class AnalysisOptions
 {
-    public required string AnalysisDirectory { get; init; }
-    public required string SearchPattern { get; init; }
-    public required bool RecursiveSearch { get; init; }
-    public required OutputFormats Formats { get; init; } = OutputFormats.Console;
+    public required string AnalysisDirectory { get; set; }
+    public required string SearchPattern { get; set; }
+    public required bool RecursiveSearch { get; set; }
+    public required OutputFormats Formats { get; set; } = OutputFormats.Console;
 }
--- Consyzer/Options/AppOptions.cs
+++ Consyzer/Options/AppOptions.cs
@@ -2,16 +2,16 @@
 
 internal sealed class AppOptions
 {
-    public required ReportOptions Report {  get; init; }
+    public required ReportOptions Report {  get; set; }
 
     public sealed class ReportOptions
     {
-        public required CsvOptions Csv { get; init; }
+        public required CsvOptions Csv { get; set; }
 
         public sealed class CsvOptions
         {
-            public required string Delimiter { get; init; }
-            public required string? Encoding { get; init; }
+            public required string Delimiter { get; set; }
+            public required string? Encoding { get; set; }
         }
     }
 
